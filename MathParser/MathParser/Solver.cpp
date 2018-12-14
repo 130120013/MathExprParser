8cf56e4390f245de69938e3a3fc7fe3f024d5856
Andrei Chusov <lpsztemp@gmail.com>
2018-12-14 22:58:50 +1000

    refactoring; troubles with std::advance

--- MathParser/MathParser/Solver.cpp
+++ MathParser/MathParser/Solver.cpp
@@ -1,8 +1,9 @@
 #include "Parser.h"
+#include <iostream>
 
 int main()
 {
-	const char funcHeader[] = "f(x) = ";
+	/*const char funcHeader[] = "f(x) = ";
 	int length1 = 4;
 	char *endptr;
 	Header<double> header(funcHeader, sizeof(funcHeader) - 1, &endptr);
@@ -11,10 +12,17 @@ int main()
 	std::vector<double> v;
 	v.push_back(1);
 	mathexpr.init_variables(v);
-	auto res = simplify<double>(mathexpr.get_body());
+	auto res = simplify(mathexpr.get_body());
 	v[0] = 2;
 	mathexpr.clear_variables();
 	mathexpr.init_variables(v);
-	res = simplify<double>(mathexpr.get_body());
+	res = simplify(mathexpr.get_body());*/
+	std::string expression = "f(x) = sin(x) + 5";
+	Mathexpr<double> mathexpr = {expression};
+	std::vector<double> v;
+	v.push_back(1);
+	mathexpr.init_variables(v);
+	std::cout << "Value: " << mathexpr.compute() << "\n";
+
 	return 0;
 }
